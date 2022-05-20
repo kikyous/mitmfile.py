b5477ab7210426b5ee2b7a06b70efaf3ef90722e
chen <kikyous@163.com>
2022-05-20 11:31:15 +0800

    extend list option

--- mitmfile.py
+++ mitmfile.py
@@ -53,19 +53,23 @@ class MitmFile:
                     set_list(k, value)
                 elif self.get_option_specs(k) in (str, typing.Optional[str]):
                     option_maps[k] = value
-                elif self.get_option_specs(k) in (int, typing.Optional[str]):
+                elif self.get_option_specs(k) in (int, typing.Optional[int]):
                     option_maps[k] = int(value)
                 elif self.get_option_specs(k) in (bool, typing.Optional[bool]):
                     option_maps[k] = False if value in ("False", "false", "f", "F", "0", "no", "n", "NO", "N") else True
         return option_maps
     
     def apply(self, options: typing.Dict[str, typing.Any]):
-        for k, v in options.items():
+        for k in options.keys():
             if not k in self.touched_options:
                 self.touched_options[k] = getattr(ctx.options, k)
 
-        for k, v in self.touched_options.items():
-            value = options.get(k) or self.touched_options[k]
+        for k, origin_value in self.touched_options.items():
+            value = options.get(k)
+            if isinstance(value, list):
+                value.extend(origin_value)
+                value = list(set(value))
+            value = value or origin_value
             try:
                 ctx.options.update(**{k: value})
             except:
